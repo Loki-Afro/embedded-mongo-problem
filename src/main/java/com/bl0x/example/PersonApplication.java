9825e545bd5a627b106a21b29a9a0ae1a8507ad0
Phillip Wirth <schaum@kaffeekrone.de>
2017-01-28 01:09:04 +0100

    yeaaaaaaaah spring ... <3

--- src/main/java/com/bl0x/example/PersonApplication.java
+++ src/main/java/com/bl0x/example/PersonApplication.java
@@ -1,5 +1,10 @@
 package com.bl0x.example;
 
+import com.bl0x.example.entities.Person;
+import com.bl0x.example.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
@@ -9,8 +14,27 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableConfigurationProperties(MongoProperties.class)
 @EnableScheduling
-public class PersonApplication {
+public class PersonApplication implements ApplicationRunner{
+
+
+	@Autowired
+	private PersonRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
+	}
+
+
+	@Override
+	public void run(final ApplicationArguments args) throws Exception {
+		Person p = new Person();
+		p.setName("Loki-Afro");
+		repository.insert(p);
+
+		Person person = repository.findAll().get(0);
+
+		System.out.println(person.getName() + " is a great Person!");
+
+		repository.deleteAll();
 	}
 }
